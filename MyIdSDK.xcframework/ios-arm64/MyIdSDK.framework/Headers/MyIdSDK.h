4df085ae558de2d1492e7e75a6ec8f7a62e56764
Javohir Savriy <138580372+javokhirsavriev@users.noreply.github.com>
2025-10-23 18:43:19 +0500

    Add files via upload

--- MyIdSDK.xcframework/ios-arm64/MyIdSDK.framework/Headers/MyIdSDK.h
+++ MyIdSDK.xcframework/ios-arm64/MyIdSDK.framework/Headers/MyIdSDK.h
@@ -16,3 +16,20 @@ FOUNDATION_EXPORT const unsigned char MyIdSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <MyIdSDK/PublicHeader.h>
 
 
+struct CallbackEntry {
+    union {
+        void (*funcWithString)(const char* secret, void* context);
+        void (*func)(void* context);
+    } callbacks;
+    void* context;
+};
+
+struct CallbackContainer {
+    struct CallbackEntry SecretCallback;
+    struct CallbackEntry FinishCallback;
+    struct CallbackEntry ErrorCallback;
+};
+
+int CMain(const char* session, const struct CallbackContainer* callbacks);
+int CClose(void);
+int CSendExtraData(const char* data, int length);
